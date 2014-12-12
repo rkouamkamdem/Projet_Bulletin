253f1c6811e8bedd3cdd1e660959d8bc42f625cc
rkouamkamdem <rkouamkamdem@yahoo.fr>
2014-12-12 12:03:34 +0100

    Commit de la MatièreDAO.

--- Bulletin it/src/com/itparis/b3/project/bulletin/dao/MatiereDAO.java
+++ Bulletin it/src/com/itparis/b3/project/bulletin/dao/MatiereDAO.java
@@ -9,7 +9,12 @@ import com.itparis.b3.project.bulletin.beans.Matiere;
 
 public class MatiereDAO {
 	
-		//Ligne rajoute pour des tests sur eclipses
+	/**
+	 * Lister l'ensemble des matères existantes en base.
+	 * @param Aucun
+	 * @return : retourne une liste des matières 
+	 * @throws SQLException
+	 */
 		public static ArrayList<Matiere> getAllMatiere() throws SQLException 
 		{
 			
@@ -40,6 +45,36 @@ public class MatiereDAO {
 			DBAction.DBClose();
 			return listMatiere;
 		}
-	
-	
+		
+		
+		/**
+		 * Lister une matère en particulier d'après son identifiant passé en paramètre de la fonction.
+		 * @param String id
+		 * @return : retourne l'objet matiereTemp 
+		 * @throws SQLException
+		 */
+		public static Matiere getMatiereById(String id) throws SQLException
+		{
+			//Instanciation de mon objet Matiere
+			Matiere matiereTemp = new Matiere();
+			
+			String req = "SELECT idMatiere, nom, Module_idModule, Coefficient FROM matiere WHERE id="+id+" ";
+			
+			// Connexion
+			DBAction.DBConnexion();
+			
+			// exécution de la requête et init
+			DBAction.setRes(DBAction.getStm().executeQuery(req));
+			
+			while( DBAction.getRes().next() )
+			{
+				// Creation de l'objet matiereTemp à travers le ResultSet BD
+				matiereTemp.setId(DBAction.getRes().getString(1));
+				matiereTemp.setNom(DBAction.getRes().getString(2));
+				matiereTemp.setIdModule(DBAction.getRes().getString(3));
+				matiereTemp.setCoefficient(DBAction.getRes().getString(4));
+			}
+			return matiereTemp;
+		}
+		
 }
